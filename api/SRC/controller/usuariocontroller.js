33ee0753b462ccf0a019378065815bea70e86182
joao victor andrade <jvas2005@outlook.com>
2022-05-30 11:47:14 -0300

    foi?

---
+++ api/SRC/controller/usuariocontroller.js
@@ -0,0 +1,19 @@
+import { login } from '../repository/usuariorepository.js';
+
+import { Router } from 'express';
+const server = Router();
+
+server.post( '/usuario/login', async (req, resp) => {
+    try{
+        const { email, senha } = req.bory;
+        const resposta = await login(email, senha);
+        
+        resp.send({
+            msg: resposta]
+        })
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+} )
